2a8ec004b90f104c8a9b14ccd055014aad545393
OhBeomho <tyler0322@naver.com>
2022-07-22 16:23:04 +0900

    log

--- main.js
+++ main.js
@@ -8,17 +8,17 @@ app.set("port", process.env.PORT || 5000)
 app.use(express.static(__dirname))
 
 const server = http.createServer(app).listen(app.get("port"), () => {
-    console.log(`[${dayjs().format("YYYY-MM-DD HH:mm:ss")}] SERVER STARTED. PORT: ${app.get("port")}`)
+    log(`[${dayjs().format("YYYY-MM-DD HH:mm:ss")}] SERVER STARTED. PORT: ${app.get("port")}`)
 })
 
 const io = socketIO(server)
-console.log(`[${dayjs().format("YYYY-MM-DD HH:mm:ss")}] WAITING FOR CONNECTION...`)
+log(`[${dayjs().format("YYYY-MM-DD HH:mm:ss")}] WAITING FOR CONNECTION...`)
 
 const users = []
 
 io.on("connection", socket => {
     let id = users.length
-    console.log(`[${dayjs().format("YYYY-MM-DD HH:mm:ss")}] CLIENT ${id} CONNECTED.`)
+    log(`[${dayjs().format("YYYY-MM-DD HH:mm:ss")}] CLIENT ${id} CONNECTED.`)
     let username = ""
 
     socket.on("name", data => {
@@ -31,7 +31,7 @@ io.on("connection", socket => {
         users.push(data.message)
         socket.emit("name", "SUCCESS")
         io.emit("server", username + "님이 들어왔습니다.")
-        console.log(`[${dayjs().format("YYYY-MM-DD HH:mm:ss")}] CLIENT ${id}: SET NAME = ${data.message}`)
+        log(`[${dayjs().format("YYYY-MM-DD HH:mm:ss")}] CLIENT ${id}: SET NAME = ${data.message}`)
     })
     socket.on("chatting", data => {
         const {
@@ -46,7 +46,7 @@ io.on("connection", socket => {
     })
     socket.on("disconnect", () => {
         io.emit("server", username + "님이 나갔습니다.")
-        console.log(`[${dayjs().format("YYYY-MM-DD HH:mm:ss")}] CLIENT DISCONNECTED. INFO:`, socket.request.connection._peername)
+        log(`[${dayjs().format("YYYY-MM-DD HH:mm:ss")}] CLIENT DISCONNECTED. INFO:`, socket.request.connection._peername)
         users.splice(users.indexOf(username), 1)
     })
 })
